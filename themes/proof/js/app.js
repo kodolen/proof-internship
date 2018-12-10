6ab19c125f6c759dcbaf6ed121b38b1c72b448fe
kodolen <git-sync@trilby.media>
2018-12-10 11:44:52 +0100

    (Grav GitSync) Automatic Commit from kodolen

--- themes/proof/js/app.js
+++ themes/proof/js/app.js
@@ -32,81 +32,86 @@ $(document).ready(function () {
             scrollTop: $('.intro-content').offset().top}, 1000);
     });
 
+    //Accordions
+    $accordionButton.click(function (e) {
+        e.preventDefault();
+
+        let $this = $(this);
+        let $text = $this.prev('p');
+
+        console.log($text);
+
+        if ($text.hasClass('accordion-text')) {
+
+            console.log('open');
+            $text.removeClass('accordion-text');
+            $text.removeClass('hidden');
+
+        }
+
+        else {
+
+            console.log('close');
+            $text.addClass('accordion-text');
+            $text.addClass('hidden');
+
+        }
+
+    });
+
+    //Scroll detection for navigation
+    $window.on('scroll', function () {
+
+        let scrollTop = $window.scrollTop();
+
+        if (scrollTop > 100) {
+
+            console.log('ja');
+            $('body').addClass('scrolled');
+
+        }
+
+        else {
+
+            console.log('nee');
+            $('body').removeClass('scrolled');
+
+        }
+
+        if (scrollTop > $('.home-visual').height() - 50) {
+
+            $body.addClass(('changeColor'));
+
+        }
+
+        else {
+
+            $body.removeClass('changeColor');
+
+        }
+
+    });
+
+    //Opens and closes hamburger menu
+    $menuToggle.click(function () {
+
+        if (menuOpen === false) {
+
+            $body.addClass('menu-open');
+            menuOpen = true;
+
+        }
+
+        else {
+
+            $body.removeClass('menu-open');
+            menuOpen = false;
+
+        }
+
+    });
+
 });
 
-$window.on('scroll', function () {
 
-    let scrollTop = $window.scrollTop();
-
-    if (scrollTop > 100) {
-
-        console.log('ja');
-        $('body').addClass('scrolled');
-
-    }
-
-    else {
-
-        console.log('nee');
-        $('body').removeClass('scrolled');
-
-    }
-
-    if (scrollTop > $('.home-visual').height() - 50) {
-
-        $body.addClass(('changeColor'));
-
-    }
-
-    else {
-
-        $body.removeClass('changeColor');
-
-    }
-
-});
-
-$menuToggle.click(function () {
-
-    if (menuOpen === false) {
-
-        $body.addClass('menu-open');
-        menuOpen = true;
-
-    }
-
-    else {
-
-        $body.removeClass('menu-open');
-        menuOpen = false;
-
-    }
-
-});
-
-$accordionButton.click(function (e) {
-    e.preventDefault();
-
-    let $this = $(this);
-    let $text = $this.prev('p');
-
-    console.log($text);
-
-    if ($text.hasClass('accordion-text')) {
-
-        console.log('open');
-        $text.removeClass('accordion-text');
-        $text.removeClass('hidden');
-
-    }
-
-    else {
-
-        console.log('close');
-        $text.addClass('accordion-text');
-        $text.addClass('hidden');
-
-    }
-
-});
 
